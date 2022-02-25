7669486a694bc8b895a0d87faa947507c7f67226
DendyAR <anugerah700@gmail.com>
2022-02-25 20:05:44 +0700

    Add new readme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,27 +8,33 @@ export default function Header() {
         <div className="logo">
             <h1><img src={img1} className="img-thumb" alt=""/>Coffee Shop</h1>
         </div>
-        <div>
+        <div className="bar">
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+        </div>
+
+        <div className="link-to">
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/products">Products</Link></li>
                 <li><Link to="/your-chart">Your Chart</Link></li>
-                <li><Link to="/historyChart">History</Link></li>
+                <li><Link className="link-to" to="/historyChart">History</Link></li>
             </ul>
         </div>
         <ul>
             <li>
-                <Link to="/login">
+                <Link className="link-to" to="/login">
                     <FaSearch/>
                 </Link>
             </li>
             <li>
-                <Link to="/login">
+                <Link className="link-to" to="/login">
                     <FaSignInAlt/>Login
                 </Link>
             </li>
             <li>
-                <Link to="/register">
+                <Link className="link-to" to="/register">
                     <FaUser/>Register
                 </Link>
             </li>
